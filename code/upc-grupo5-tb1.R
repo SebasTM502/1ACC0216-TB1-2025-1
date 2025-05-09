5f72c030689d2bb8b240716855d4d97654c0dfb4
SebasTM502 <tucutansigma99@gmail.com>
2025-05-09 11:46:55 -0500

    Update upc-grupo5-tb1.R

--- code/upc-grupo5-tb1.R
+++ code/upc-grupo5-tb1.R
@@ -123,6 +123,135 @@ df$company <- NULL
 # Verificamos los valores nulos nuevamente
 colSums(is.na(df)) 
 
+# Resumen de las variables Categoricas, en cual observamos la cantidad y porcentaje por cada variable
+
+cat_vars <- names(df)[sapply(df, function(x) class(x) %in% c("character", "factor"))]
+for (var in cat_vars) {
+  cat("\nResumen de:", var, "\n")
+  print(table(df[[var]]))
+  print(prop.table(table(df[[var]])))
+}
+
+
+#- Deteccion de Outliers:
+
+#Visualizar outliers con boxplots
+boxplot(df$lead_time, main = "Outliers en lead_time", horizontal = TRUE)
+boxplot(df$adr, main = "Outliers en ADR", horizontal = TRUE)
+boxplot(df$adults, main = "Outliers en adultos", horizontal = TRUE)
+
+#- Tratamiento de Outliers
+
+# Calculos para definir los cuartiles
+identify_outliers <- function(x) {
+  Q1 <- quantile(x, .25, na.rm=TRUE)
+  Q3 <- quantile(x, .75, na.rm=TRUE)
+  IQR <- Q3 - Q1
+  lower <- Q1 - 1.5*IQR
+  upper <- Q3 + 1.5*IQR
+  x[x < lower | x > upper]
+}
+
+# Definimos la columnas a analizar
+num_vars <- c("lead_time", "adr", "children", "babies", "adults")
+for (v in num_vars) {
+  cat("\n--- Variable:", v, "---\n")
+  print(summary(df[[v]]))
+  boxplot(df[[v]], main = paste("Boxplot de", v))
+}
+
+# Definimos y aplicamos el Winsorización
+winsorize <- function(x, probs = c(0.01, 0.99)) {
+  qs <- quantile(x, probs, na.rm = TRUE)
+  x[x < qs[1]] <- qs[1]
+  x[x > qs[2]] <- qs[2]
+  x
+}
+
+for (col in num_vars) {
+  df[[col]] <- winsorize(df[[col]], probs = c(0.01, 0.99))
+}
+
+# Resumen de los resultados
+summary(df[, num_vars])
+par(mfrow = c(2, 3))
+for (v in num_vars) {
+  boxplot(df[[v]], main = paste("Boxplot de", v))
+}
+par(mfrow = c(1, 1))
+
+for (v in num_vars) {
+  n_out <- length(identify_outliers(df[[v]]))
+  cat("Outliers en", v, ":", n_out, "\n")
+}
+
+#- Creación de nuevas columnas
+
+## Creamos una columna fecha que una los datos arrival_date_year,arrival_date_month y arrival_date_day_of_month en arrival_date
+
+meses <- c("January", "February", "March", "April", "May", "June", 
+           "July", "August", "September", "October", "November", "December")
+
+# Convertir los meses de texto a números
+df$arrival_date_month_num <- match(df$arrival_date_month, meses)
+
+# Crear la nueva columna de fecha
+df$arrival_date <- as.Date(paste(df$arrival_date_year, df$arrival_date_month_num, df$arrival_date_day_of_month, sep = "-"), "%Y-%m-%d")
+
+# Eliminar la columna temporal de mes numérico
+df$arrival_date_month_num <- NULL
+
+# Verificar la nueva columna
+head(df$arrival_date)
+
+# Crear variable de estadía total 'stay total'
+df$stay_total <- df$stays_in_weekend_nights + df$stays_in_week_nights
+
+# Revision de los datos LIMPIOS
+
+summary(df)
+
+colSums(is.na(df)) 
+
+sapply(df,class)
+
+# Guardamos la data limpia 
+write.csv(df, "hotel_bookings_limpio.csv", row.names = FALSE)
+
+
+# ----- VISUALIZACION DE DATOS -----
+
+cat('¿Cuántas reservas se realizan por tipo de hotel? ¿Qué tipo de hotel prefiere la gente?')
+
+
+g1<- ggplot(df,aes(x= hotel ,fill = hotel)) +
+  geom_bar()+
+  geom_text(stat = "count", aes(label = ..count..), vjust = -0.5) +
+  labs(title = "Cantidad de Reservas por Tipo de Hotel",
+       x = "Tipo de hotel",
+       y = "Cantidad de Reservas"
+       )+ 
+  theme_cowplot()
+g1
+
+
+cat('¿Está aumentando la demanda con el tiempo?')
+
+
+df$reserva_fecha <- as.Date(paste(df$arrival_date_year, df$arrival_date_month, 1, sep = "-"), format = "%Y-%B-%d")
+
+reservas_mensuales <- df %>%
+  group_by(reserva_fecha = floor_date(reserva_fecha, "month")) %>%
+  summarise(cantidad = n())
+
+ggplot(reservas_mensuales, aes(x = reserva_fecha, y = cantidad)) +
+  geom_line(color = "#33a02c") +
+  labs(title = "Evolución de Reservas en el Tiempo", 
+       x = "Fecha", 
+       y = "Cantidad de Reservas") +
+  theme_minimal()
+                             
+
 
 
 
